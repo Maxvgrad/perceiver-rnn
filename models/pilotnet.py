4b218983309036bc244c5dccbc594f5690a03c10
Gordei Pribõtkin <pribytkingordei@gmail.com>
2024-04-28 12:50:47 +0300

    Added ReLu and flatten

--- models/pilotnet.py
+++ models/pilotnet.py
@@ -5,15 +5,24 @@ class PilotNet(nn.Module):
         super().__init__()
         self.conv_stack = nn.Sequential(
             nn.Conv2d(3, 24, kernel_size=5, stride=2),
+            nn.ReLU(),
             nn.Conv2d(24, 36, kernel_size=5, stride=2),
+            nn.ReLU(),
             nn.Conv2d(36, 48, kernel_size=5, stride=2),
+            nn.ReLU(),
             nn.Conv2d(48, 64, kernel_size=3, stride=0),
+            nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, stride=0),
+            nn.ReLU(),
+            nn.Flatten(),
         )
         self.linear_stack = nn.Sequential(
             nn.LazyLinear(100),
+            nn.ReLU(),
             nn.Linear(100, 50), 
+            nn.ReLU(),
             nn.Linear(50, 10),
+            nn.ReLU(),
             nn.Linear(10, 1)
         )
 
