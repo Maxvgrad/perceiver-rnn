76d49bd4ff6901c609e4e6fbb8243d283078770e
Gordei Pribõtkin <pribytkingordei@gmail.com>
2024-05-13 22:37:00 +0300

    change sbatch slurm directory

--- evaluate_slurm.sh
+++ evaluate_slurm.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# NOTE: before running this do `conda activate itn2`.
+# NOTE: before running this do `conda activate vista`.
+# Example 'sbatch evaluate_slurm.sh --model pilotnet.onnx --traces trace_forward --traces-root ./traces/ --save-video'
 # run as `sbatch vista_evaluate.sh`
 
 # Job Details
 #SBATCH --partition=gpu
 #SBATCH -J vista
+#SBATCH -o ./vista_runs/%j-slurm-run.txt # STDOUT/STDERR
 
 # Resources
 #SBATCH -t 23:59:00
