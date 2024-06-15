0671ceab22cf93b1cfe4786e298def30f1bbdb4f
Gordei Pribõtkin <pribytkingordei@gmail.com>
2024-06-15 13:20:26 +0300

    fix parameters

--- experiment.py
+++ experiment.py
@@ -186,8 +186,8 @@ def train(train_config):
         pmodel = Perceiver(
             input_channels = 3,          # number of channels for each token of the input
             input_axis = 2,              # number of axis for input data (2 for images, 3 for video)
-            #num_freq_bands = 6,          # number of freq bands, with original value (2 * K + 1)
-            #max_freq = 10.,              # maximum frequency, hyperparameter depending on how fine the data is
+            num_freq_bands = 6,          # number of freq bands, with original value (2 * K + 1)
+            max_freq = 10.,              # maximum frequency, hyperparameter depending on how fine the data is
             depth = 1,                   # depth of net. The shape of the final attention mechanism will be:
                                          #   depth * (cross attention -> self_per_cross_attn * self attention)
             num_latents = 256,           # number of latents, or induced set points, or centroids. different papers giving it different names
