93c0d00cf33f04cad1af451f385a61a071442cac
Gordei Pribõtkin <pribytkingordei@gmail.com>
2024-06-15 12:50:47 +0300

    change perceiver sbatch and exp

--- experiment.py
+++ experiment.py
@@ -186,17 +186,17 @@ def train(train_config):
         pmodel = Perceiver(
             input_channels = 3,          # number of channels for each token of the input
             input_axis = 2,              # number of axis for input data (2 for images, 3 for video)
-            num_freq_bands = 6,          # number of freq bands, with original value (2 * K + 1)
-            max_freq = 10.,              # maximum frequency, hyperparameter depending on how fine the data is
+            #num_freq_bands = 6,          # number of freq bands, with original value (2 * K + 1)
+            #max_freq = 10.,              # maximum frequency, hyperparameter depending on how fine the data is
             depth = 1,                   # depth of net. The shape of the final attention mechanism will be:
                                          #   depth * (cross attention -> self_per_cross_attn * self attention)
             num_latents = 256,           # number of latents, or induced set points, or centroids. different papers giving it different names
             latent_dim = 512,            # latent dimension
             cross_heads = 1,             # number of heads for cross attention. paper said 1
-            latent_heads = 8,            # number of heads for latent self attention, 8
+            latent_heads = 4,            # number of heads for latent self attention, 8
             cross_dim_head = 64,         # number of dimensions per cross attention head
             latent_dim_head = 64,        # number of dimensions per latent self attention head
-            num_classes = 1,          # output numb"er of classes
+            num_classes = 1,             # output number of classes
             attn_dropout = 0.,
             ff_dropout = 0.,
             weight_tie_layers = False,   # whether to weight tie layers (optional, as indicated in the diagram)
