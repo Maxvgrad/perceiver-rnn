81c4a9ac39b901231e59825f7f60af3e4671b713
Maksim Ploter <maksim.ploter@gmail.com>
2024-07-15 12:06:33 +0300

    fix: change bbox format

--- datasets/transforms.py
+++ datasets/transforms.py
@@ -1,3 +1,4 @@
+from utils.box_ops import box_xyxy_to_cxcywh
 
 
 class NormalizeBoxes(object):
@@ -7,5 +8,5 @@ class NormalizeBoxes(object):
             boxes = target["boxes"].float()
             boxes[..., 0::2] /= w
             boxes[..., 1::2] /= h
-            target["boxes"] = boxes
+            target["boxes"] = box_xyxy_to_cxcywh(boxes)
         return image, target
